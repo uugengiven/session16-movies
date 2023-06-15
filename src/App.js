aa66b6d595a0ce559e4ed3f17ed3b410e340d6f4
John Lange <lange.john.m@gmail.com>
2023-06-14 21:14:27 -0400

    Initial Comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    loadMovies()
+  }, []) // that empty array at the end means it should only run once on startup. This is in fact a lie, it actually runs twice due to "react things"
+
+  const loadMovies = () => {
+    const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNmZiYzdmM2YzMTNiZDM5NTkwMmFmNDY0ZWY0NzI2MiIsInN1YiI6IjRmZWRmZDAyNzYwZWUzNjBhODAwMDNiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AT_RzLoLZM1RG3jPuNQSWjbGGHdKO8B_LsH7sq6VO48'
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer ' + token
+      }
+    };
+
+    fetch(url, options) // first go to the page and wait
+      .then(res => res.json()) // then get the result and return the json
+      .then(json => setMovies(json.results)) // push movies into the state (results holds the movies)
+      .catch(err => console.error('error:' + err)); // oh noes!
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {movies.map(movie => {
+          return <li>{movie.title}</li>
+        })}
+      </ul>
     </div>
   );
 }
